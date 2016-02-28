4a115914447537ba943eda1dd7cbb49a464d410f
YuraDuda <yuraduda890@gmail.com>
2016-02-28 15:49:19 +0200

    Initial commit

---
+++ src/main/java/com/yura/springboot/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.yura.springboot;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yura on 28.02.16.
+ */
+
+@Component
+public class HelloWorld {
+
+    public String Say(){
+        return  "lalalalal";
+    }
+}
---
+++ src/main/java/com/yura/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.yura.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SpringbootApplication.class, args);
+		HelloWorld hw = ctx.getBean(HelloWorld.class);
+		System.out.print(hw.Say());
+
+	}
+}
